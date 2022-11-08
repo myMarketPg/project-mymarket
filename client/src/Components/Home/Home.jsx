f8847fa9567559979ec3a368287c2eec424bd5f2
MZacca <mzacca.95@gmail.com>
2022-11-08 17:02:41 -0300

    componente-home

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Footer from '../Footer/footer';
+import NavBar from '../Navbar/NavBar'
 
 const Home = () => {
   return (
     <div>
+      <NavBar />
+      <div className='body'>
         <h1>Bienvenido a My Market!</h1>
+        <h2>Creá tu propia tienda y potencia tu negocio!</h2>
+      </div>
+      <Footer />
     </div>
   )
 }
