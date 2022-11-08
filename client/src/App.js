4172169935e098c38b7858ae7ea8910d7f6d6c03
Andyvaz1 <andyvaz192@gmail.com>
2022-11-08 18:30:02 -0300

    App.js ruta / agregada que renderiza Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
-  );
+import { Home } from "./Components/Home/Home";
+
+function App(props) {
+    return (
+        <div>
+            <Switch>
+                <Route path="/" exact render={() => <Home />} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
