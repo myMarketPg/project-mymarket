05d5aebcf76c6caef55f4ce49debc48f86b23fa1
Facu-J-Boy <boyfacundo45@gmail.com>
2022-11-08 17:00:31 -0300

    correccion de errores

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -4,6 +4,12 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('user', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
