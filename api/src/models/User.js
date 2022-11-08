084b96901c0ee46f4513edca342a11bf39631a0a
Facu-J-Boy <boyfacundo45@gmail.com>
2022-11-08 15:50:11 -0300

    Facundo/back-models-User

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -4,9 +4,22 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    password: {
+      type: DataTypes.STRING,
+      set(value) {
+        this.setDataValue('password', hash(this.username + value));
+      },
+      allowNull: false,
+    }
   });
 };
