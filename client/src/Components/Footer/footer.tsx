c514736e857fe70d21ce1859af06c507d19b0204
Andyvaz1 <andyvaz192@gmail.com>
2022-11-09 15:45:28 -0300

    Bootstrap instalado, integrado con SASS, falta ajustar jerarquia de estilos

--- client/src/Components/Footer/footer.tsx
+++ client/src/Components/Footer/footer.tsx
@@ -1,38 +1,39 @@
 // import { Link } from "react-router-dom";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
         <footer>
             <div className="social">
-                <a href="#">
+                <a href="https://www.instagram.com">
                     <i className="icon ion-social-instagram"></i>
                 </a>
-                <a href="#">
+                <a href="https://www.snapchat.com">
                     <i className="icon ion-social-snapchat"></i>
                 </a>
-                <a href="#">
+                <a href="https://www.twitter.com">
                     <i className="icon ion-social-twitter"></i>
                 </a>
-                <a href="#">
+                <a href="https://www.facebook.com">
                     <i className="icon ion-social-facebook"></i>
                 </a>
             </div>
             <ul className="list-inline">
                 <li className="list-inline-item">
-                    <a href="#">Home</a>
+                    <Link to="/">Home</Link>
                 </li>
                 <li className="list-inline-item">
-                    <a href="#">Servicios</a>
+                    <Link to="/servicios">Servicios</Link>
                 </li>
                 <li className="list-inline-item">
-                    <a href="#">Sobre Nosotros</a>
+                    <Link to="/sobrenosotros">Sobre Nosotros</Link>
                 </li>
                 <li className="list-inline-item">
-                    <a href="#">Términos</a>
+                    <Link to="/terminos">Términos</Link>
                 </li>
                 <li className="list-inline-item">
-                    <a href="#">Politicas de Privacidad</a>
+                    <Link to="/politicas">Politicas de Privacidad</Link>
                 </li>
             </ul>
             <p className="copyright">MyMarket © 2022</p>
