535d7158a514fa263c60b54628359355cfc03f98
Facu-J-Boy <boyfacundo45@gmail.com>
2022-11-10 15:59:18 -0300

    cambios

---
+++ api/src/models/Buyer.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('buyer', {
+    id: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      set(value) {
+        this.setDataValue('password', hash(this.username + value));
+      },
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false
+  }
+  );
+};
--- api/src/routes/controllers/users/index.js
+++
@@ -1,8 +0,0 @@
-const { User } = require('../../../db');
-
-module.exports = {
-    listUsers: async () => {
-        const users = await User.findAll();
-        return users;
-    }
-}
