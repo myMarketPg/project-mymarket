ee8050f5ab88f761f813d112b00a1cb93c90c589
Santiago L. Romero <santiago_pna@hotmail.com>
2022-11-11 20:47:03 -0300

    santi4246/Back-model-Buyer

--- api/src/models/Buyer.js
+++ api/src/models/Buyer.js
@@ -14,6 +14,10 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,      
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
