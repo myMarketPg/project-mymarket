b83cae1870fa7f184bcdc9c29a40dbd7a8879f27
Facu-J-Boy <boyfacundo45@gmail.com>
2022-11-10 23:30:29 -0300

    rutas de buyers modularizadas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 // Importación de todos los routers;
+const findBuyerMiddleware = require('./findBuyer');
 const buyerMiddleware = require('./Buyer');
+const postBuyerMiddleware = require('./post_Buyer');
 const productMiddleware = require('./Product');
 const sellerMiddleware = require('./Seller');
 const postSellerMiddleware = require('./post_Seller');
@@ -9,7 +11,9 @@ const postAdminMiddleware = require('./post_admin');
 const router = Router();
 
 // Configuración de los routers
-router.use('/buyers', buyerMiddleware);
+router.use('/findBuyer', findBuyerMiddleware);
+router.use('/buyer', buyerMiddleware);
+router.use('/buyer', postBuyerMiddleware);
 router.use('/product', productMiddleware);
 router.use('/seller', sellerMiddleware);
 router.use('/seller', postSellerMiddleware);
