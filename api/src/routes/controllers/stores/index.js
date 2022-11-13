51d563392ab82d8264e1253a6446416eb8a56fe5
Facu-J-Boy <boyfacundo45@gmail.com>
2022-11-13 18:00:02 -0300

    cambios en la tabla y rutas de store

--- api/src/routes/controllers/stores/index.js
+++ api/src/routes/controllers/stores/index.js
@@ -5,13 +5,17 @@ module.exports = {
         const stores = await Store.findAll();
         return stores;
     },
-    postStore: async (banner, logo, phoneNumber, email, adress) => {
+    postStore: async (banner, logo, phoneNumber, email, adress, country, city, state, zip) => {
         const creatStore = Store.create({
             banner: banner,
             logo: logo,
             phoneNumber: phoneNumber,
             email: email,
-            adress: adress
+            adress: adress,
+            country: country,
+            city: city,
+            state: state,
+            zip: zip
         });
     }
 }
