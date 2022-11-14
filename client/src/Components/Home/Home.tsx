2afdf65c4f131bf959b0cf2d1dc4f754f19e43d4
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2022-11-14 10:48:04 -0300

    correccion a cambios basicos para la home

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import Footer from "../Footer/Footer";
+import Footer from "../Footer/footer";
 import NavBar from "../Navbar/NavBar";
 import Componente from "../ComponentePruebaRedux/Componente";
 
