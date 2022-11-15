d64539f372bb760014b2698303c986c7ece6c126
Santiago Rambeaud <santy_r_7@hotmail.com>
2022-11-15 18:38:05 -0300

    carrito de compras

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -329,4 +329,12 @@ export const deleteOrder = (id) => {
     }
 }
 
+/////----CARRITO DE COMPRAS----/////
+export const TYPES = {
+    ADD_TO_CART:  "ADD_TO_CART",
+    REMOVE_ONE_FROM_CART:"REMOVE_ONE_FROM_CART",
+    REMOVE_ALL_FROM_CART:"REMOVE_ALL_FROM_CART",
+    CLEAR_CART:"CLEAR_CART"
+};
+
 /////-----FILTROS-----/////
