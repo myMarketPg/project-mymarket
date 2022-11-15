1bffd1ab429db727e01ded8dac3fed402f1e0246
Andyvaz1 <andyvaz192@gmail.com>
2022-11-15 11:56:28 -0300

    minor fixes

--- client/src/Components/SellerProfile/ManageComponents/Products/SellerManageProducts.jsx
+++ client/src/Components/SellerProfile/ManageComponents/Products/SellerManageProducts.jsx
@@ -33,6 +33,7 @@ export default function SellerManageProducts() {
     useEffect(() => {
         setLocalProducts(allProducts);
     }, [allProducts]);
+
     console.log(allProducts);
     /// RENDER ///
     return (
