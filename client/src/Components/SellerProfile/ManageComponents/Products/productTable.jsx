19714962ba42fc145a37afca2e0b1995b1f4f5f0
Andyvaz1 <andyvaz192@gmail.com>
2022-11-15 20:16:54 -0300

    Cambio de titulo de la web - Sume Active a product list - formConact terminado- Estilos para elementos de listas

--- client/src/Components/SellerProfile/ManageComponents/Products/productTable.jsx
+++ client/src/Components/SellerProfile/ManageComponents/Products/productTable.jsx
@@ -15,19 +15,21 @@ export default function ProductTable({ localProducts }) {
                     <tr>
                         <th scope="col">#id</th>
                         <th scope="col">Name</th>
+                        <th scope="col"></th>
                         <th scope="col">Category</th>
                         <th scope="col">Price</th>
                         <th scope="col">Stock</th>
                         <th scope="col">Sales</th>
+                        <th scope="col">Active</th>
                         <th scope="col"></th>
                     </tr>
                 </thead>
-                <tbody className="table-group-divider">
+                <tbody className="table-group-divider ">
                     {localProducts?.map((product) => {
                         return (
                             <tr key={product.id}>
-                                <td>{product.id}</td>
-                                <td>
+                                <td className="align-middle">{product.id}</td>
+                                <td className="align-middle">
                                     {" "}
                                     <button
                                         type="button"
@@ -38,14 +40,53 @@ export default function ProductTable({ localProducts }) {
                                         {product.name}
                                     </button>
                                 </td>
-                                <td>{product.category}</td>
-                                <td>{`$${product.price}`}</td>
-                                <td>{product.stock}</td>
-                                <td>{product.sales}</td>
-                                <td>
+                                <td className="align-middle">
+                                    <img
+                                        src={product.img}
+                                        className="listImg img-thumbnail"
+                                    />
+                                </td>
+                                <td className="align-middle ">
+                                    {product.category}
+                                </td>
+                                <td className="align-middle">{`$${product.price}`}</td>
+                                <td className="align-middle">
+                                    {product.stock}
+                                </td>
+                                <td className="align-middle">
+                                    {product.sales}
+                                </td>
+                                {product.active ? (
+                                    <td className="align-middle">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="20"
+                                            height="20"
+                                            fill="green"
+                                            className="bi bi-check-circle-fill listIcon"
+                                            viewBox="0 0 16 16"
+                                        >
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+                                        </svg>
+                                    </td>
+                                ) : (
+                                    <td className="align-middle">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="20"
+                                            height="20"
+                                            fill="red"
+                                            className="bi bi-x-circle-fill listIcon"
+                                            viewBox="0 0 16 16"
+                                        >
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path>
+                                        </svg>
+                                    </td>
+                                )}
+                                <td className="align-middle">
                                     <button
                                         type="button"
-                                        className="btn btn-success"
+                                        className="btn btn-success listButton"
                                         data-bs-toggle="modal"
                                         data-bs-target="#editProductModal"
                                     >
@@ -68,7 +109,7 @@ export default function ProductTable({ localProducts }) {
 
                                     <button
                                         type="button"
-                                        className="btn btn-danger "
+                                        className="btn btn-danger listButton "
                                         data-bs-toggle="modal"
                                         data-bs-target="#deleteProductModal"
                                     >
