35cb99f2c5acb0b9d96b55b25f9c9a2f232ae097
Santiago L. Romero <santiago_pna@hotmail.com>
2022-11-15 23:50:04 -0300

    Updated files from branch santi4246

--- client/src/Components/About/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import style from './Button.module.css';
-
-export default function Button () {
-    return (
-        <Link to = '/'><button className = {style.btn}>Return Home</button></Link>
-    )
-}
--- client/src/Components/SellerProfile/ManageComponents/SellerManageOrders.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-
-export default function SellerManageOrders() {
-    const dispatch = useDispatch();
-    return <div></div>;
-}
