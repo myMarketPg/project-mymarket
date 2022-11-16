613c10103e5fc39c52fca154d18015777df7be7b
Facu-J-Boy <boyfacundo45@gmail.com>
2022-11-15 21:03:09 -0300

    facu/front-component-auth

---
+++ client/src/Components/Auth/LoginButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+
+    const {loginWithRedirect} = useAuth0();
+
+  return (
+    <button 
+    type="button" 
+    class="btn btn-outline-dark" 
+    onClick={() => loginWithRedirect()}> Log in </button>
+  )
+}
+
+export default LoginButton
